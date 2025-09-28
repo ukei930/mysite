ca229a523d807af5c9d13c0165e13827d6e706fb
鈴木夏大 <t233025@edu.kaiyodai.ac.jp>
2025-09-28 17:46:03 +0900

    Refactor locale handling in i18n configuration to use requestLocale

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,11 +4,15 @@ import {getRequestConfig} from 'next-intl/server';
 // Can be imported from a shared config
 const locales = ['ja', 'en', 'zh'];
 
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async ({requestLocale}) => {
+  // This function can be marked `async` if you're doing async work here
+  let locale = await requestLocale;
+
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
